2b7e310f916eea37d5c6e839451d1afd094a498d
Лиана Гатауллина <Gataullina.lg@yandex.ru>
2022-08-07 19:40:26 +0300

    update repositories

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Categories, Comment, Genres, Review, Title, User
+
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Genres)
+admin.site.register(Categories)
+admin.site.register(Title)
